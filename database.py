7e995e5cf778640c80130d5b88d1aed24ba0af1c
beep13 <97983228+beep13@users.noreply.github.com>
2022-06-19 00:31:00 -0700

    gn

--- database.py
+++ database.py
@@ -1,1 +1,12 @@
-# Nothing here yet!
+# title, release_date, watched
+
+CREATE_BOOKS_TABLE = """CREATE TABLE IF NOT EXISTS books(
+    title TEXT,
+    release_timestamp REAL,
+    watched INTEGER
+);"""
+
+INSERT_BOOKS = "INSERT INTO books (titles, release_timestamp, watched) VALUES (?, ?, 0);"
+SELECT_ALL_BOOKS = "SELECT * FROM books;"
+SELECT_UPCOMING_BOOKS = "SELECT * FROM books WHERE release_timestamp > ?;"
+SELECT_READ_BOOKS = "SELECT * FROM movies WHERE watched = 1;"
