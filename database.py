4f0cef7c3668087edcfc4f3e442928aed700e029
beep13 <97983228+beep13@users.noreply.github.com>
2022-06-19 12:21:50 -0700

    database.py

--- database.py
+++ database.py
@@ -1,4 +1,6 @@
-# title, release_date, watched
+import datetime
+import sqlite3
+from urllib.parse import ParseResultBytes
 
 CREATE_BOOKS_TABLE = """CREATE TABLE IF NOT EXISTS books(
     title TEXT,
@@ -10,3 +12,35 @@ INSERT_BOOKS = "INSERT INTO books (titles, release_timestamp, watched) VALUES (?
 SELECT_ALL_BOOKS = "SELECT * FROM books;"
 SELECT_UPCOMING_BOOKS = "SELECT * FROM books WHERE release_timestamp > ?;"
 SELECT_READ_BOOKS = "SELECT * FROM movies WHERE watched = 1;"
+
+connection = sqlite3.connect("data.db")
+
+def create_tables():
+    with connection:
+        connection.execute(CREATE_BOOKS_TABLE)
+
+
+def add_book(title, release_timestamp):
+    with connection:
+        connection.execute(INSERT_BOOKS, (title, release_timestamp))
+
+
+def get_books(upcoming=False):
+    with connection:
+        cursor = connection.cursor()
+        if upcoming:
+            today_timestamp = datetime.datetime.today().timestamp()
+            cursor.execute(SELECT_UPCOMING_BOOKS, (today_timestamp,))
+        else:
+            cursor.execute(SELECT_ALL_BOOKS)
+        return cursor.fetchall()
+
+
+def read_book(title):
+    pass
+
+def get_read_books():
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SELECT_READ_BOOKS)
+        return cursor.fetchall()
