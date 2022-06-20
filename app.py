581ad22a40bde286e5c3dc2e2f56da80bdb31ec5
beep13 <97983228+beep13@users.noreply.github.com>
2022-06-19 23:10:31 -0700

    1.0 running

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+import datetime
 import database
 
 menu = """Please select one of the following options:
-1) Add new movie.
-2) View upcoming movies.
-3) View all movies
-4) Watch a movie
-5) View watched movies.
+1) Add new book.
+2) View your to-read list.
+3) View all books
+4) Read a book
+5) View read books.
 6) Exit.
 
 Your selection: """
@@ -15,16 +16,43 @@ welcome = "Welcome to the reading list app!"
 print(welcome)
 database.create_tables()
 
+
+def prompt_add_book():
+    title = input("Book title: ")
+    release_date = input("Publish date (dd-mm-YYYY): ")
+    parsed_date = datetime.datetime.strptime(release_date, "%d-%m-%Y")
+    timestamp = parsed_date.timestamp()
+
+    database.add_book(title, timestamp)
+
+
+def print_book_list(heading, books):
+    print(f"-- {heading} books --")
+    for book in books:
+        book_date = datetime.datetime.fromtimestamp(book[1])
+        human_date = book_date.strftime("%b %d %Y")
+        print(f"{book[0]} (published on {human_date})")
+    print("---- \n")
+
+
+def prompt_read_book():
+    book_title = input("Enter book title you've read: ")
+    database.read_book(book_title)
+
+
 while (user_input := input(menu)) != "6":
     if user_input == "1":
-        pass
+        prompt_add_book()
     elif user_input == "2":
-        pass
+        books = database.get_books(True)
+        print_book_list("Upcoming", books)
     elif user_input == "3":
-        pass
+        books = database.get_books()
+        print_book_list("All", books)
     elif user_input == "4":
-        pass
+        prompt_read_book()
     elif user_input == "5":
-        pass
+        books = database.get_read_books()
+        print_book_list("Read", books)
     else:
         print("Invalid input, please try again!")
--- database.py
+++ database.py
@@ -8,7 +8,7 @@ CREATE_BOOKS_TABLE = """CREATE TABLE IF NOT EXISTS books(
     watched INTEGER
 );"""
 
-INSERT_BOOKS = "INSERT INTO books (titles, release_timestamp, watched) VALUES (?, ?, 0);"
+INSERT_BOOKS = "INSERT INTO books (title, release_timestamp, watched) VALUES (?, ?, 0);"
 SELECT_ALL_BOOKS = "SELECT * FROM books;"
 SELECT_UPCOMING_BOOKS = "SELECT * FROM books WHERE release_timestamp > ?;"
 SELECT_READ_BOOKS = "SELECT * FROM books WHERE watched = 1;"
